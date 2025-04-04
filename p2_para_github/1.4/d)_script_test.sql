8eb6467b2a392fdbf1b2ad6601cea4d96f58922b
Pablo <100451061@alumnos.uc3m.es>
2025-04-04 03:56:14 +0200

    pruebas acabadas

--- p2_para_github/1.4/d)_script_test.sql
+++ p2_para_github/1.4/d)_script_test.sql
@@ -1,6 +1,55 @@
---------------------------------------------------------------------------------------------------------------------------------
--- Trigger (c)
--- lo pruebo. 1ro verifico que el usuario exista
+-- (a) Evitar posts de bibliotecas (tipo = 'L')
+
+-- 1. Busca usuarios institucionales (bibliotecas)
+SELECT user_id, name, type
+FROM users
+WHERE type = 'L';
+
+-- 2. Intenta insertar un post (debe lanzar error)
+INSERT INTO posts (signature, user_id, stopdate, post_date, text, likes, dislikes)
+VALUES ('OC886', '9994309605', DATE '2024-11-19', SYSDATE, 'Esto es una prueba', 0, 0);
+
+-- 3. Intenta modificar un post de biblioteca (también fallará)
+UPDATE posts
+SET text = 'Modificado por biblioteca'
+WHERE user_id = '9994309605';
+----------------------------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------------------
+-- (b) Deterioro activa fecha de baja
+
+-- 1. Resetea el estado de una copia:
+UPDATE copies
+SET condition    = 'G', -- good
+    deregistered = NULL
+WHERE signature = 'UD561';
+
+--  Verifica el estado:
+select *
+from copies
+WHERE signature = 'UD561';
+
+
+-- 2. Cambia el estado a 'D' (deteriorado) y activa el trigger:
+UPDATE copies
+SET condition = 'D'
+WHERE signature = 'UD561';
+
+
+-- 3. Verifica que la fecha de baja (deregistered) se ha asignado con hora:
+SELECT signature,
+       isbn,
+       condition,
+       comments,
+       TO_CHAR(deregistered, 'DD/MM/YYYY HH24:MI:SS') AS fecha_baja_con_hora
+FROM copies
+WHERE signature = 'UD561';
+----------------------------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------------------
+-- (c) Históricos al eliminar usuario
+
+-- 1. Verifica que el usuario existe y tiene préstamos:
 SELECT *
 FROM users
 WHERE user_id = '0230880540';
@@ -9,63 +58,16 @@ SELECT *
 FROM loans
 WHERE user_id = '0230880540';
 
-
--- lo borro
+-- 2. Elimina el usuario (activará el trigger trg_before_delete_users):
 DELETE
 FROM users
 WHERE user_id = '0230880540';
 
-select *
-from users_hist; -- los datos del borrado se muestran en users_hist (ha funcionado)
+-- 3. Comprueba que se han movido a históricos:
+SELECT *
+FROM users_hist
+WHERE user_id = '0230880540';
 
-select *
-from loans_hist;
--- los datos del borrado se muestran en loans_hist (ha funcionado)
-
-
---------------------------------------------------------------------------------------------------------------------------------
--- Trigger (b)
-UPDATE copies
-SET condition    = 'G',
-    deregistered = NULL
-WHERE signature = 'UD561';
-
-
-UPDATE copies
-SET condition = 'D'
-WHERE signature = 'UD561';
-
-SELECT signature,
-       condition,
-       TO_CHAR(deregistered, 'DD/MM/YYYY HH24:MI:SS') AS fecha_baja
-FROM copies
-WHERE condition = 'D';
-
---------------------------------------------------------------------------------------------------------------------------------
--- Trigger (a)
-SELECT user_id, name, type
-FROM users
-WHERE type = 'L'
-  AND ROWNUM = 1;
-
--- Output: el trigger impedirá el INSERT y lanzará: ERROR: Las bibliotecas no pueden publicar posts.
-INSERT INTO posts (signature, user_id, stopdate, post_date, text, likes, dislikes)
-VALUES ('OC886', '9994309605', DATE '2024-11-19', SYSDATE, 'Esto es una prueba', 0, 0);
-
-
--- buscamos
-SELECT user_id, name, type
-FROM users
-WHERE type = 'L';
--- 'L' = Library
-
--- [9994309605 ,9994309606,9994309607,9994309608]]
-
--- Output: el trigger impedirá el INSERT y lanzará: ERROR: Las bibliotecas no pueden publicar posts.
-INSERT INTO posts (signature, user_id, stopdate, post_date, text, likes, dislikes)
-VALUES ('OC886', '9994309607', DATE '2024-11-19', SYSDATE, 'Post de biblioteca', 0, 0);
-
--- Output: el trigger impedirá el UPDATE y lanzará: ERROR: Las bibliotecas no pueden modificar posts.
-UPDATE posts
-SET text = 'Modificado por biblioteca'
-WHERE user_id = '9994309605'
+SELECT *
+FROM loans_hist
+WHERE user_id = '0230880540';
